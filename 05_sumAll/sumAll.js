ad42f41cf6ed5e803aaabc6827122f990ee31a7d
Wantonfury <orzan.alexandru94@gmail.com>
2022-06-26 18:30:09 +0300

    Finish fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(l, r) {
+    let sum = 0;
+    
+    if ((l < 0 || r < 0) || (typeof(l) !== 'number' || typeof(r) !== 'number')) return 'ERROR';
+    
+    if (l > r) {
+        let t = l;
+        l = r;
+        r = t;
+    }
+    
+    for (let i = l; i <= r; ++i) sum += i;
+    
+    return sum;
 };
 
 // Do not edit below this line
