865d25979cb453e96a5a53f193f599643c2e7d55
Wantonfury <orzan.alexandru94@gmail.com>
2022-06-26 18:17:55 +0300

    Finish fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
-
+    let arr = [];
+    
+    if (arguments.length == 1) return arguments[0];
+    
+    for (let i = 0; i < arguments[0].length; ++i) {
+        let add = 1;
+        
+        for (let j = 1; j < arguments.length; ++j) {
+            if (arguments[0][i] === arguments[j]) add = 0;
+        }
+        
+        if (add) arr.push(arguments[0][i]);
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
