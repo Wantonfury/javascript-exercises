261fd152ced544308e7273a1c167a7a2cccc7877
Wantonfury <orzan.alexandru94@gmail.com>
2022-07-28 03:14:58 +0300

    Finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+  if (f <= 1) return 1;
+  
+  let total = 2;
+	for (let i = 3; i <= f; ++i)
+    total = total * i;
+  return total;
 };
 
 // Do not edit below this line
