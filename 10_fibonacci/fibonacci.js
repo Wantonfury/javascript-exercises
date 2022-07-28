9c2cbf6cc0752d7bdab081166528515891e2066e
Wantonfury <orzan.alexandru94@gmail.com>
2022-07-28 04:55:24 +0300

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(f) {
+    if (f < 0) return "OOPS";
+    if (f <= 2) return 1;
+    
+    let a = 1, b = 1;
+    for (let i = 3; i <= f; ++i) {
+        [a, b] = [b, a + b];
+    }
+    return b;
 };
 
 // Do not edit below this line
