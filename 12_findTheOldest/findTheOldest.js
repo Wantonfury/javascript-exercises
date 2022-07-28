984a0d818f71118e2e6a88ac78017b05ba73babb
Wantonfury <orzan.alexandru94@gmail.com>
2022-07-28 05:32:27 +0300

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const getAge = (yearOfDeath, yearOfBirth) => {
+    if (yearOfDeath) return yearOfDeath - yearOfBirth;
+    return (new Date()).getFullYear() - yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, p) => {
+        if ("name" in oldest === false) {
+            oldest["name"] = p.name;
+            oldest["age"] = getAge(p.yearOfDeath, p.yearOfBirth);
+        } else {
+            let age = getAge(p.yearOfDeath, p.yearOfBirth);
+            
+            if (oldest.age < age) {
+                oldest["name"] = p.name;
+                oldest["age"] = age;
+            }
+        }
+        
+        return oldest;
+    }, {});
 };
 
 // Do not edit below this line
