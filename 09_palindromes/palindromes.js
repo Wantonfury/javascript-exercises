6c5a0ab3249256bb1c2e0d069dd61ff72763a4b1
Wantonfury <orzan.alexandru94@gmail.com>
2022-07-28 03:27:41 +0300

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = palindrome.replace(/[!, .]/g, "");
+    palindrome = palindrome.toLowerCase();
+    let half = Math.floor(palindrome.length / 2);
+    
+    for (let i = 0; i < half; ++i) {
+        if (palindrome[i] != palindrome[palindrome.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
